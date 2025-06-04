741c792df9dd04ed9607ce0e2cdc66bee432cddb
STHEXA <ti.syu.s@gmail.com>
2025-06-04 22:30:32 +0900

    middleware削除

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-// export const middleware = createNextAuthMiddleware();
-
-// export const config = {
-//   matcher: ["/(.*)"], // Replace this with your own matcher logic
-// };
